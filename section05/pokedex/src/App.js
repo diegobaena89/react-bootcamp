52a1aa5777ba9fd9f6dd8d178f815e1d9a8f45f5
diegobaena89 <diegobf.cello@gmail.com>
2021-03-15 10:54:03 -0300

    add pokedex app

---
+++ section05/pokedex/src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+import './App.css';
+import Pokedex from './Pokedex';
+function App() {
+  return (
+    <div className="App">
+      <Pokedex />
+      <div className="Pokedex-card">
+      </div>
+    </div>
+  );
+}
+
+export default App;
