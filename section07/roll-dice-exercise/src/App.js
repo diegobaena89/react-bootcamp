129d7359e6731850b43ed492e16f2f6cc9c6a19e
diegobaena89 <diegobf.cello@gmail.com>
2021-03-19 17:50:18 -0300

    add dice exercise

---
+++ section07/roll-dice-exercise/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import './Die.css'
+import RollDice from './RollDice';
+
+function App() {
+  return (
+    <div className="App">
+      <RollDice />
+    </div>
+  );
+}
+
+export default App;
